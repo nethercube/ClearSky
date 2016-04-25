b196413e2bc8dcf22607ecfb2137cbf5b04d31dc
thebigsmileXD <thebigsmilexd@gmail.com>
2016-04-25 15:03:36 +0200

    AI manager fix (2)

--- src/pocketmine/entity/ai/AIManager.php
+++ src/pocketmine/entity/ai/AIManager.php
@@ -5,6 +5,7 @@ namespace pocketmine\entity\ai;
 use pocketmine\entity\Entity;
 
 abstract class AIManager{
+	private static $knownAIs = [];
 
 	public function __construct(Entity $entity){
 		$this->startAI($entity);
@@ -12,14 +13,17 @@ abstract class AIManager{
 
 	public function startAI(Entity $entity){}
 
+	public static function registerAIs($aiclassName, $className, $force = false){
 		$class = new \ReflectionClass($className);
 		if(is_a($className, Entity::class, true) and !$class->isAbstract()){
 			if($className::NETWORK_ID !== -1){
+				self::$knownAIs[$className::NETWORK_ID] = $className;
 			}
 			elseif(!$force){
 				return false;
 			}
 			
+			self::$knownAIs[$class->getShortName()] = $className;
 			self::$shortNames[$className] = $class->getShortName();
 			return true;
 		}
--- src/pocketmine/level/ai/MoveCalculaterTask.php
+++ src/pocketmine/level/ai/MoveCalculaterTask.php
@@ -1,41 +1,42 @@
 <?php
+
 namespace pocketmine\level\ai;
 
 use pocketmine\Server;
 use pocketmine\level\Level;
 use pocketmine\scheduler\AsyncTask;
+use pocketmine\entity\ai\AIManager;
 
 class MoveCalculaterTask extends AsyncTask{
 	private $server;
 	private $levelId;
 	private $entityId;
 	private $entityType;
-	
+
 	public function __construct(Level $level, $levelId, $entityId, $entityType){
 		$this->server = $level;
 		$this->levelId = $levelId;
 		$this->entityId = $entityId;
 		$this->entityType = $entityType;
 	}
-	
+
 	public function onRun(){
-		//$rs = "LevelId: ".$this->levelId." EntityId: ".$this->entityId." EntityType: ".$this->entityType;
+		// $rs = "LevelId: ".$this->levelId." EntityId: ".$this->entityId." EntityType: ".$this->entityType;
 		$entity = $this->server->getEntity($this->entityId);
-		AIManager::calculateMovement($entity);
+		// AIManager::calculateMovement($entity);
 		$x = $entity->x;
 		$y = $entity->y;
 		$z = $entity->z;
 		$yaw = $entity->yaw;
 		$pitch = $entity->pitch;
-		$rs = ['id'=>$this->entityId,'x'=>$x,'y'=>$y,'z'=>$z,'yaw'=>$yaw,'pitch'=>$pitch];
+		$rs = ['id' => $this->entityId, 'x' => $x, 'y' => $y, 'z' => $z, 'yaw' => $yaw, 'pitch' => $pitch];
 		$this->setResult($rs, false);
 	}
-	
+
 	public function onCompletion(Server $server){
 		$ai = $server->getLevel($this->levelId)->getAI();
 		if($ai instanceof AI and $this->hasResult()){
 			$ai->moveCalculationCallback($this->getResult());
 		}
 	}
-	
 }
